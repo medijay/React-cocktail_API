3847d1f3b4e3258b2f01395c7f13b180ae89a95c
Ahmed Adekunle <medijay99@yahoo.com>
2023-04-19 19:41:10 +0100

    First commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia illo
+        quisquam consequuntur facere. Nihil nisi voluptatum quisquam a tempora
+        corrupti quas rerum ab, velit, error dolorum iusto reiciendis quibusdam
+        sunt.
+      </p>
+    </section>
+  );
+};
+
+export default About;
